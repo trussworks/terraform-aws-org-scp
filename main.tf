74c5b4006c94618264befa725f7ae05fb37be66d
Michael Kania <dynamike@truss.works>
2020-02-26 16:27:50 -0800

    add deny effect to the root policy

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ data "aws_iam_policy_document" "deny_root_account" {
   statement {
     actions   = ["*"]
     resources = ["*"]
+    effect    = "Deny"
     condition {
       test     = "StringLike"
       variable = "aws:PrincipalArn"
